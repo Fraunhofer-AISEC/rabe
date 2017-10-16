4794d2d65244afba3be858f1d72da855231caf4d
Schanzenbach, Martin <martin.schanzenbach@aisec.fraunhofer.de>
2017-10-16 15:32:12 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -4,40 +4,69 @@ use std::string::String;
 
 pub struct AbePublicKey
 {
-    pkey: f32
+    _pkey: f32
 }
 
 pub struct AbeMasterKey
 {
-    msk: f32
+    _msk: f32
 }
 
 pub struct AbeSecretKey
 {
-    sk: f32
+    _sk: f32
 }
 
-pub fn abe_setup () -> (AbePublicKey,AbeMasterKey)
+pub fn abe_setup () -> (Option<AbePublicKey>,Option<AbeMasterKey>)
 {
-    let pk = AbePublicKey { pkey: 0.0 };
-    let msk = AbeMasterKey { msk: 0.0 };
+    let pk = AbePublicKey { _pkey: 0.0 };
+    let msk = AbeMasterKey { _msk: 0.0 };
 
     
-    return (pk, msk);
+    return (None, None);
 }
 
-pub fn abe_keygen (attributes: &LinkedList<String>) -> AbeSecretKey
+pub fn abe_keygen (pk: &AbePublicKey,
+                   msk: &AbeMasterKey,
+                   attributes: &LinkedList<String>) -> Option<AbeSecretKey>
 {
-    let sk = AbeSecretKey { sk: 0.0 };
     for str in attributes.iter() {
         print!("{}", str);
     }
-    return sk;
+    return None;
+}
+
+pub fn abe_encrypt (pk: &AbePublicKey,
+                    policy: String,
+                    plaintext: &Vec<u8>,
+                    ciphertext: &mut Vec<u8>) -> bool
+{
+    return true;
+}
+
+pub fn abe_decrypt (pk: &AbePublicKey,
+                    sk: &AbeSecretKey,
+                    ciphertext: &Vec<u8>,
+                    plaintext: &mut Vec<u8>) -> bool
+{
+    return true;
+}
+
+pub fn abe_public_key_serialize (pk: &AbePublicKey,
+                                 pk_serialized: &mut Vec<u8>) -> bool
+{
+    return true;
+}
+
+pub fn abe_public_key_deserialize (pk_data: &Vec<u8>) -> Option<AbePublicKey>
+{
+    return None;
 }
 
 #[cfg(test)]
 mod tests {
     use abe_setup;
+    use abe_keygen;
     use AbePublicKey;
     use AbeMasterKey;
     use std::collections::LinkedList;
@@ -50,10 +79,20 @@ mod tests {
     #[test]
     fn test_setup() {
         let (pk,msk) = abe_setup ();
+        assert!(!pk.is_none());
+        assert!(!msk.is_none());
+    }
+    fn test_keygen() {
+        //let (pk,msk) = abe_setup ();
         let mut attrs: LinkedList<String> = LinkedList::new();
         attrs.push_back(String::from("a1"));
         attrs.push_back(String::from("a2"));
         attrs.push_back(String::from("a3"));
-        //assert!((pk, msk) = abe_setup (&attrs));
+        match abe_setup () {
+            (Some(pk),Some(msk)) => { let sk = abe_keygen (&pk,&msk,&attrs);
+                                     assert!(!sk.is_none()); },
+            _ => assert!(false)
+        }
+        //assert_ne!(None, sk);
     }
 }
