a22f5b2f4492adcc4345f9a671756147b5dd138b
Georg Räß <georg.raess@aisec.fraunhofer.de>
2017-10-20 10:43:35 +0200

    added SK struct draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,47 @@
 // Barreto-Naehrig (BN) curve construction with an efficient bilinear pairing e: G1 × G2 → GT
 extern crate bn;
 extern crate rand;
+extern crate sha3;
+extern crate byteorder;
 use std::collections::LinkedList;
 use std::string::String;
 use bn::*;
 use sha3::{Digest, Sha3_256};
+use byteorder::{ByteOrder, BigEndian};
 //use rand::Rng;
 
-pub struct AbePublicKey
-{
+pub struct AbePublicKey {
     _h: bn::G2,
     _h1: bn::G2,
     _h2: bn::G2,
     _t1: bn::Gt,
-    _t2: bn::Gt
+    _t2: bn::Gt,
 }
 
-pub struct AbeMasterKey
-{
-
+pub struct AbeMasterKey {
     _g: bn::G1,
     _h: bn::G2,
     _a1: bn::Fr,
     _a2: bn::Fr,
     _b1: bn::Fr,
     _b2: bn::Fr,
-    _g_b1: bn::G1,
-    _g_b2: bn::G1,
-    _g_b3: bn::G1
+    _g_d1: bn::G1,
+    _g_d2: bn::G1,
+    _g_d3: bn::G1,
 }
 
-pub struct AbeSecretKey
-{
-    _sk: f32
+pub struct AbeSecretKeyAttribute {
+    _sk_i1: bn::G1,
+    _sk_i2: bn::G1,
+    _sk_i3: bn::G1,
 }
 
-pub fn abe_setup () -> (AbePublicKey, AbeMasterKey)
-{
+pub struct AbeSecretKey {
+    _sk0: Vec<bn::G2>,
+    _ski: Vec<AbeSecretKeyAttribute>,
+}
+
+pub fn abe_setup() -> (AbePublicKey, AbeMasterKey) {
     // random number generator
     let rng = &mut rand::thread_rng();
     // generator of group G1: g and generator of group G2: h
@@ -54,10 +59,16 @@ pub fn abe_setup () -> (AbePublicKey, AbeMasterKey)
     let h2 = h * a2;
 
     // calculate pairing for T1 and T2
-    let t1 = pairing (g, h).pow(d1 * a1 + d3);
-    let t2 = pairing (g, h).pow(d2 * a2 + d3);
+    let t1 = pairing(g, h).pow(d1 * a1 + d3);
+    let t2 = pairing(g, h).pow(d2 * a2 + d3);
 
-    let pk = AbePublicKey { _h: h, _h1: h1, _h2: h2, _t1: t1, _t2: t2};
+    let pk = AbePublicKey {
+        _h: h,
+        _h1: h1,
+        _h2: h2,
+        _t1: t1,
+        _t2: t2,
+    };
 
     // generate b1,b2 from Z*_p (*means it must not be null, can we be sure?)
     let b1 = Fr::random(rng);
@@ -67,21 +78,25 @@ pub fn abe_setup () -> (AbePublicKey, AbeMasterKey)
     let g_b1 = g * d1;
     let g_b2 = g * d2;
     let g_b3 = g * d3;
-    let msk = AbeMasterKey { _g: g, _h: h,
-                             _a1: a1, _a2: a2,
-                             _b1: b1, _b2: b2,
-                             _g_b1: g_b1,
-                             _g_b2: g_b2,
-                             _g_b3: g_b3};
-
-
+    let msk = AbeMasterKey {
+        _g: g,
+        _h: h,
+        _a1: a1,
+        _a2: a2,
+        _b1: b1,
+        _b2: b2,
+        _g_d1: g_b1,
+        _g_d2: g_b2,
+        _g_d3: g_b3,
+    };
     return (pk, msk);
 }
 
-pub fn abe_keygen (pk: &AbePublicKey,
-                   msk: &AbeMasterKey,
-                   attributes: &LinkedList<String>) -> Option<AbeSecretKey>
-{
+pub fn abe_keygen(
+    pk: &AbePublicKey,
+    msk: &AbeMasterKey,
+    attributes: &LinkedList<String>,
+) -> Option<AbeSecretKey> {
     // random number generator
     //generate random t
     for str in attributes.iter() {
@@ -90,24 +105,25 @@ pub fn abe_keygen (pk: &AbePublicKey,
     return None;
 }
 
-fn bn_hash (str: String)
-{
+fn bn_hash(str: String) {
     //Precomp. TODO make constants
-    let sqrt3 = 3.0.sqrt();
-    let j = (-1 + sqrt3) / 2;
+    let sqrt3 = 3.0_f64.sqrt();
+    let j = (-1_f64 + sqrt3) / 2_f64;
     let mut hasher = Sha3_256::default();
-    
-    hasher.input(str);
-    let t = BigEndian::read_i32(hasher.result());
+
+    hasher.input(str.as_bytes());
+    let hash = hasher.result();
+    let t = BigEndian::read_i32(&hash);
     //Main computation
 
 }
 
-pub fn abe_encrypt (pk: &AbePublicKey,
-                    tags: &LinkedList<String>,
-                    plaintext: &Vec<u8>,
-                    ciphertext: &mut Vec<u8>) -> bool
-{
+pub fn abe_encrypt(
+    pk: &AbePublicKey,
+    tags: &LinkedList<String>,
+    plaintext: &Vec<u8>,
+    ciphertext: &mut Vec<u8>,
+) -> bool {
     // random number generator
     let rng = &mut rand::thread_rng();
 
@@ -128,22 +144,20 @@ pub fn abe_encrypt (pk: &AbePublicKey,
     return true;
 }
 
-pub fn abe_decrypt (pk: &AbePublicKey,
-                    sk: &AbeSecretKey,
-                    ciphertext: &Vec<u8>,
-                    plaintext: &mut Vec<u8>) -> bool
-{
+pub fn abe_decrypt(
+    pk: &AbePublicKey,
+    sk: &AbeSecretKey,
+    ciphertext: &Vec<u8>,
+    plaintext: &mut Vec<u8>,
+) -> bool {
     return true;
 }
 
-pub fn abe_public_key_serialize (pk: &AbePublicKey,
-                                 pk_serialized: &mut Vec<u8>) -> bool
-{
+pub fn abe_public_key_serialize(pk: &AbePublicKey, pk_serialized: &mut Vec<u8>) -> bool {
     return true;
 }
 
-pub fn abe_public_key_deserialize (pk_data: &Vec<u8>) -> Option<AbePublicKey>
-{
+pub fn abe_public_key_deserialize(pk_data: &Vec<u8>) -> Option<AbePublicKey> {
     return None;
 }
 
@@ -162,16 +176,16 @@ mod tests {
     }
     #[test]
     fn test_setup() {
-        let (pk,msk) = abe_setup ();
+        let (pk, msk) = abe_setup();
     }
     #[test]
     fn test_keygen() {
-        let (pk,msk) = abe_setup ();
+        let (pk, msk) = abe_setup();
         let mut attrs: LinkedList<String> = LinkedList::new();
         attrs.push_back(String::from("a1"));
         attrs.push_back(String::from("a2"));
         attrs.push_back(String::from("a3"));
-        let sk = abe_keygen (&pk,&msk,&attrs);
+        let sk = abe_keygen(&pk, &msk, &attrs);
         assert!(!sk.is_none());
         //assert_ne!(None, sk);
     }
