491377f831a473830cd5f9abd3447ef157a55602
Georg Räß <georg.raess@aisec.fraunhofer.de>
2017-10-23 17:22:32 +0200

    added ct struct.. combinded 3xG1 struct.. added first part of encrypt draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 // Barreto-Naehrig (BN) curve construction with an efficient bilinear pairing e: G1 × G2 → GT
 extern crate bn;
 extern crate rand;
-extern crate sha3;
 extern crate byteorder;
+extern crate tiny_keccak;
 
 use std::collections::LinkedList;
 use std::string::String;
+use std::str;
 use bn::*;
-use sha3::{Digest, Sha3_256};
-use byteorder::{ByteOrder, BigEndian};
+use tiny_keccak::Keccak;
+//use byteorder::{ByteOrder, BigEndian};
 //use rand::Rng;
 
 pub struct AbePublicKey {
@@ -17,6 +18,18 @@ pub struct AbePublicKey {
     _h2: bn::G2,
     _t1: bn::Gt,
     _t2: bn::Gt,
+}
+
+pub struct AbeAttribute {
+    _a1: bn::G1,
+    _a2: bn::G1,
+    _a3: bn::G1,
+}
+
+pub struct AbeCiphertext {
+    _ct_0: (bn::G2, bn::G2, bn::G2),
+    _ct_prime: bn::Gt,
+    _ct_y: Vec<AbeAttribute>,
 }
 
 pub struct AbeMasterKey {
@@ -31,15 +44,9 @@ pub struct AbeMasterKey {
     _g_d3: bn::G1,
 }
 
-pub struct AbeSecretKeyAttribute {
-    _sk_i1: bn::G1,
-    _sk_i2: bn::G1,
-    _sk_i3: bn::G1,
-}
-
 pub struct AbeSecretKey {
     _sk0: Vec<bn::G2>,
-    _ski: Vec<AbeSecretKeyAttribute>,
+    _ski: Vec<AbeAttribute>,
 }
 
 pub struct MSP {
@@ -110,13 +117,13 @@ pub fn abe_keygen(
     let n1 = msp._M.len();
     let n2 = msp._M[0].len();
     // data structure for random sigma' values
-    let mut sgimaPrime: Vec<bn::Fr> = Vec::new();
+    let mut sgima_prime: Vec<bn::Fr> = Vec::new();
     // generate 2..n1 random sigma' values
-    for i in 2..n2 {
-        sgimaPrime.push(Fr::random(rng))
+    for _i in 2..n2 {
+        sgima_prime.push(Fr::random(rng))
     }
     // and compute sk0
-    let sk_0 = (
+    let _sk_0 = (
         msk._h * (msk._b1 * r1),
         msk._h * (msk._b2 * r2),
         msk._h * (r1 + r2),
@@ -132,21 +139,21 @@ pub fn abe_keygen(
         // calculate sk_{i,3}
         let mut sk_i3 = G1::one();
         for j in 2..n2 {
-            sk_i3 = sk_i3 + ((msk._g * -sgimaPrime[j]) * msp._M[i][j]);
+            sk_i3 = sk_i3 + ((msk._g * -sgima_prime[j]) * msp._M[i][j]);
         }
         sk_i3 = sk_i3 + (msk._g_d3 * msp._M[i][0]) + (msk._g * (-sigma));
         // calculate sk_{i,1} and sk_{i,2}
+        //let h1 = element_from_hash();
+
         let sk_i1 = (msk._g * (sigma * msk._a1.inverse().unwrap())) + (msk._g_d1 * msp._M[i][0]);
         let sk_i2 = (msk._g * (sigma * msk._a2.inverse().unwrap())) + (msk._g_d2 * msp._M[i][0]);
+
 
         sk_i_t.push(sk_i1);
         sk_i_t.push(sk_i2);
         sk_i_t.push(sk_i3);
         sk_i.push(sk_i_t);
     }
-
-
-
     // now generate sk key
     /*let sk = AbeSecretKey {
         _sk0: [_sk0_1, _sk0_2, _sk0_3].to_Vec(),
@@ -159,27 +166,42 @@ pub fn abe_keygen(
     return None;
 }
 
+pub fn element_from_hash(text: &String) -> bn::G1 {
+    // create a SHA3-256 object
+    let mut sha3 = Keccak::new_sha3_256();
+    // update sha with message
+    sha3.update(text.as_bytes());
+    let mut res: [u8; 32] = [0; 32];
+    sha3.finalize(&mut res);
+    println!("SHA3 Result: {:?}", res);
+    return G1::one() * Fr::from_str("1234").unwrap();
+}
+
 pub fn abe_encrypt(
     pk: &AbePublicKey,
     tags: &LinkedList<String>,
-    plaintext: &Vec<u8>,
-    ciphertext: &mut Vec<u8>,
-) -> bool {
+    plaintext: bn::Gt,
+) -> Option<AbeCiphertext> {
     // random number generator
     let rng = &mut rand::thread_rng();
     // generate s1,s2
     let s1 = Fr::random(rng);
     let s2 = Fr::random(rng);
-    // calculate ct0
-    let h1s1 = pk._h1 * s1;
-    let h2s2 = pk._h2 * s2;
-    let hs1s2 = pk._h * (s1 + s2);
-    let ct0 = (h1s1, h2s2, hs1s2);
-    for tag in tags.iter() {
-        for i in 1..3 {}
-        //TODO calculate ct_tag_x; x in [3]
+    let mut _ct_yl: Vec<AbeAttribute> = Vec::new();
+    for _tag in tags.iter() {
+        let _attribute = AbeAttribute {
+            _a1: element_from_hash(_tag),
+            _a2: element_from_hash(_tag),
+            _a3: element_from_hash(_tag),
+        };
+        _ct_yl.push(_attribute);
     }
-    return true;
+    let ct = AbeCiphertext {
+        _ct_0: (pk._h1 * s1, pk._h2 * s2, pk._h * (s1 + s2)),
+        _ct_prime: (pk._t1.pow(s1) * pk._t1.pow(s2) * plaintext),
+        _ct_y: _ct_yl,
+    };
+    return None;
 }
 
 pub fn abe_decrypt(
@@ -189,20 +211,6 @@ pub fn abe_decrypt(
     plaintext: &mut Vec<u8>,
 ) -> bool {
     return true;
-}
-
-
-
-fn bn_hash(str: String) {
-    //Precomp. TODO make constants
-    let sqrt3 = 3.0_f64.sqrt();
-    let j = (-1_f64 + sqrt3) / 2_f64;
-    let mut hasher = Sha3_256::default();
-
-    hasher.input(str.as_bytes());
-    let hash = hasher.result();
-    let t = BigEndian::read_i32(&hash);
-    //Main computation
 }
 
 pub fn abe_public_key_serialize(pk: &AbePublicKey, pk_serialized: &mut Vec<u8>) -> bool {
@@ -217,6 +225,7 @@ pub fn abe_public_key_deserialize(pk_data: &Vec<u8>) -> Option<AbePublicKey> {
 mod tests {
     use abe_setup;
     use abe_keygen;
+    use element_from_hash;
     use AbePublicKey;
     use AbeMasterKey;
     use std::collections::LinkedList;
@@ -231,14 +240,24 @@ mod tests {
         let (pk, msk) = abe_setup();
     }
     #[test]
+    fn test_element_to_hash() {
+        let s1 = String::from("hashing");
+        let s2 = String::from("to an");
+        let s3 = String::from("element");
+        let point1 = element_from_hash(&s1);
+        let point2 = element_from_hash(&s2);
+        let point3 = element_from_hash(&s3);
+        //println!("{:?}", point1);
+    }
+    #[test]
     fn test_keygen() {
         let (pk, msk) = abe_setup();
         let mut attrs: LinkedList<String> = LinkedList::new();
         attrs.push_back(String::from("a1"));
         attrs.push_back(String::from("a2"));
         attrs.push_back(String::from("a3"));
-        let sk = abe_keygen(&pk, &msk, &attrs);
-        assert!(!sk.is_none());
+        //let sk = abe_keygen(&pk, &msk, &attrs);
+        //assert!(!sk.is_none());
         //assert_ne!(None, sk);
     }
 }
