82741fede401e534e95e52336b3f2f1994983784
Schanzenbach, Martin <martin.schanzenbach@aisec.fraunhofer.de>
2017-10-26 23:58:21 +0200

    add aes

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,14 @@ use num_bigint::BigInt;
 use bn::*;
 use crypto::digest::Digest;
 use crypto::sha3::Sha3;
+use crypto::{ symmetriccipher, buffer, aes, blockmodes };
+use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
+use bincode::SizeLimit::Infinite;
+use bincode::rustc_serialize::{encode, decode};
 //use rustc_serialize::{Encodable, Decodable};
-//use rustc_serialize::hex::{FromHex, ToHex};
+use rustc_serialize::hex::{FromHex, ToHex};
 //use byteorder::{ByteOrder, BigEndian};
-//use rand::Rng;
+use rand::Rng;
 use policy::AbePolicy;
 
 mod policy;
@@ -52,6 +56,8 @@ pub struct AbeCiphertext {
     _ct_0: (bn::G2, bn::G2, bn::G2),
     _ct_prime: bn::Gt,
     _ct_y: Vec<(bn::G1, bn::G1, bn::G1)>,
+    _ciphertext: Vec<u8>,
+    _iv: [u8; 16]
 }
 
 #[derive(RustcEncodable, RustcDecodable, PartialEq)]
@@ -217,18 +223,72 @@ pub fn hash_string_to_element(text: &String) -> bn::G1 {
     return hash_to_element(text.as_bytes());
 }
 
+fn encrypt_aes(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
+    let mut encryptor = aes::cbc_encryptor(
+        aes::KeySize::KeySize256,
+        key,
+        iv,
+        blockmodes::PkcsPadding);
+    // Each encryption operation encrypts some data from
+    // an input buffer into an output buffer. Those buffers
+    // must be instances of RefReaderBuffer and RefWriteBuffer
+    // (respectively) which keep track of how much data has been
+    // read from or written to them.
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
 
+    // Each encryption operation will "make progress". "Making progress"
+    // is a bit loosely defined, but basically, at the end of each operation
+    // either BufferUnderflow or BufferOverflow will be returned (unless
+    // there was an error). If the return value is BufferUnderflow, it means
+    // that the operation ended while wanting more input data. If the return
+    // value is BufferOverflow, it means that the operation ended because it
+    // needed more space to output data. As long as the next call to the encryption
+    // operation provides the space that was requested (either more input data
+    // or more output space), the operation is guaranteed to get closer to
+    // completing the full operation - ie: "make progress".
+    //
+    // Here, we pass the data to encrypt to the enryptor along with a fixed-size
+    // output buffer. The 'true' flag indicates that the end of the data that
+    // is to be encrypted is included in the input buffer (which is true, since
+    // the input data includes all the data to encrypt). After each call, we copy
+    // any output data to our result Vec. If we get a BufferOverflow, we keep
+    // going in the loop since it means that there is more work to do. We can
+    // complete as soon as we get a BufferUnderflow since the encryptor is telling
+    // us that it stopped processing data due to not having any more data in the
+    // input buffer.
+    loop {
+        let result = try!(encryptor.encrypt(&mut read_buffer, &mut write_buffer, true));
+
+        // "write_buffer.take_read_buffer().take_remaining()" means:
+        // from the writable buffer, create a new readable buffer which
+        // contains all data that has been written, and then access all
+        // of that data as a slice.
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => { }
+        }
+    }
+
+    Ok(final_result)
+}
 
 pub fn abe_encrypt(
     pk: &AbePublicKey,
     tags: &LinkedList<String>,
-    plaintext: bn::Gt) -> Option<AbeCiphertext> {
+    plaintext: &Vec<u8>) -> Option<AbeCiphertext> {
 
     if tags.is_empty() {
         return None;
     }
     // random number generator
     let rng = &mut rand::thread_rng();
+    //Choose random secret
+    let secret = pairing (G1::random(rng), G2::random(rng));
     // generate s1,s2
     let s1 = Fr::random(rng);
     let s2 = Fr::random(rng);
@@ -241,17 +301,63 @@ pub fn abe_encrypt(
             );
         _ct_yl.push(_attribute);
     }
-    let ct = AbeCiphertext {
-        _ct_0: (pk._h1 * s1, pk._h2 * s2, pk._h * (s1 + s2)),
-        _ct_prime: (pk._t1.pow(s1) * pk._t1.pow(s2) * plaintext),
-        _ct_y: _ct_yl,
-    };
-    return Some(ct);
+    //Encrypt plaintext using derived key from secret
+    let mut sha = Sha3::sha3_256();
+    match encode(&secret, Infinite) {
+        Err(_) => return None,
+        Ok(e) =>  {
+            sha.input(e.to_hex().as_bytes());
+            let mut key: [u8; 32] = [0;32];
+            sha.result(&mut key);
+            let mut iv: [u8; 16] = [0; 16];
+            rng.fill_bytes(&mut iv);
+            let ct = AbeCiphertext {
+                _ct_0: (pk._h1 * s1, pk._h2 * s2, pk._h * (s1 + s2)),
+                _ct_prime: (pk._t1.pow(s1) * pk._t1.pow(s2) * secret),
+                _ct_y: _ct_yl,
+                _ciphertext: encrypt_aes (plaintext, &key, &iv).ok().unwrap(),
+                _iv: iv
+            };
+            return Some(ct);
+        }
+    }
+    return None;
+}
+
+// Decrypts a buffer with the given key and iv using
+// AES-256/CBC/Pkcs encryption.
+//
+// This function is very similar to encrypt(), so, please reference
+// comments in that function. In non-example code, if desired, it is possible to
+// share much of the implementation using closures to hide the operation
+// being performed. However, such code would make this example less clear.
+fn decrypt_aes(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
+    let mut decryptor = aes::cbc_decryptor(
+            aes::KeySize::KeySize256,
+            key,
+            iv,
+            blockmodes::PkcsPadding);
+
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
+    let mut buffer = [0; 4096];
+    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+
+    loop {
+        let result = try!(decryptor.decrypt(&mut read_buffer, &mut write_buffer, true));
+        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => { }
+        }
+    }
+
+    Ok(final_result)
 }
 
 pub fn abe_decrypt(
     sk: &AbeSecretKey,
-    ct: &AbeCiphertext) -> Option<bn::Gt> {
+    ct: &AbeCiphertext) -> Option<Vec<u8>> {
     let mut ct_1 = bn::G1::one();
     let mut ct_2 = bn::G1::one();
     let mut ct_3 = bn::G1::one();
@@ -271,8 +377,21 @@ pub fn abe_decrypt(
 
     let num = ct._ct_prime * pairing (ct_1, sk._sk0.0) * pairing (ct_2, sk._sk0.1) * pairing (ct_3, sk._sk0.2);
     let den = pairing (sk_1, ct._ct_0.0) * pairing (sk_2, ct._ct_0.1) * pairing (sk_3, ct._ct_0.2);
+    let secret = num * den.inverse();
 
-    return Some(num * den.inverse());
+    //Encrypt plaintext using derived key from secret
+    let mut sha = Sha3::sha3_256();
+    match encode(&secret, Infinite) {
+        Err(_) => return None,
+        Ok(e) =>  {
+            sha.input(e.to_hex().as_bytes());
+            let mut key: [u8; 32] = [0;32];
+            sha.result(&mut key);
+            return Some (decrypt_aes(&ct._ciphertext[..], &key, &ct._iv).ok().unwrap());
+        }
+    }
+
+    return None;
 }
 
 #[cfg(test)]
