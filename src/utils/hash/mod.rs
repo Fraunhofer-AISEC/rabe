8c01c4d8eed36b4b56f40c1cb11d7f3e77f7f478
Georg Räß <georg.bramm@aisec.fraunhofer.de>
2018-04-24 16:48:44 +0200

    hash comments

--- src/utils/hash/mod.rs
+++ src/utils/hash/mod.rs
@@ -4,32 +4,20 @@ extern crate bn;
 use bn::*;
 use blake2_rfc::blake2b::blake2b;
 
-/////////////////////////////////////////////////////////////////////
-// HASH TO GROUP FUNTIONS
-/////////////////////////////////////////////////////////////////////
-
-// used to hash to G1
+/// hash a String to an element of G1 using blake2b and generator g
 pub fn blake2b_hash_g1(g: bn::G1, data: &String) -> bn::G1 {
     let hash = blake2b(64, &[], data.as_bytes());
     return g * Fr::interpret(array_ref![hash.as_ref(), 0, 64]);
 }
 
-// used to hash to G2
+/// hash a String to an element of G2 using blake2b and generator g
 pub fn blake2b_hash_g2(g: bn::G2, data: &String) -> bn::G2 {
     let hash = blake2b(64, &[], data.as_bytes());
     return g * Fr::interpret(array_ref![hash.as_ref(), 0, 64]);
 }
 
-// used to hash to Fr
+/// hash a String to Fr using blake2b
 pub fn blake2b_hash_fr(data: &String) -> Fr {
     let hash = blake2b(64, &[], data.as_bytes());
     return Fr::interpret(array_ref![hash.as_ref(), 0, 64]);
 }
-/*
-
-// generic function used to hash
-pub fn blake2b_hash<T>(g: T, data: &String) -> T {
-    let hash = blake2b(64, &[], data.as_bytes());
-    return g * Fr::interpret(array_ref![hash.as_ref(), 0, 64]);
-}
-*/
