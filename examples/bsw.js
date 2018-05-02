ef6459ef44d9ec377ca6c156d79433ae5e0ef5db
Georg Räß <georg.bramm@aisec.fraunhofer.de>
2018-05-02 13:10:32 +0200

    removed py and js examples

--- examples/bsw.js
+++
@@ -1,9 +0,0 @@
-var ffi = require('ffi');
-
-var lib = ffi.Library('../target/release/librabe', {
-  'bsw_context_create': ['void', []]
-});
-
-lib.bsw_context_create();
-
-console.log("done!");
--- examples/bsw.py
+++
@@ -1,6 +0,0 @@
-from ctypes import cdll
-
-lib = cdll.LoadLibrary("../target/release/librabe.so")
-ctx = lib.bsw_context_create()
-print(ctx)
-print("done!")
