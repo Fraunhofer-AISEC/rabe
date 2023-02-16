77b85a1e0dc5aaa07ddaa57ca9d5a1512ac87a64
Klas-Norling <klasaman99@hotmail.com>
2023-02-16 14:22:52 +0100

    Fixed comments and updated example

--- rabe_py/src/lsw/types.rs
+++ rabe_py/src/lsw/types.rs
@@ -19,25 +19,28 @@ use pyo3::exceptions::PyValueError;
 
 use crate::serializable;
 
-
+// Wrapper for LSW public key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbePublicKey {
     pub pk: KpAbePublicKey,
 }
 
+// Wrapper for LSW master key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeMasterKey {
     pub msk: KpAbeMasterKey,
 }
 
+// Wrapper for LSW secret key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeSecretKey {
     pub sk: KpAbeSecretKey,
 }
 
+// Wrapper for LSW ciphertext
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeCiphertext {
